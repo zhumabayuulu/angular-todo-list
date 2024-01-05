f6100dafb94f62b3c4f9c9269d28beb15220063a
almanbet <hialmanbet@gmail.com>
2024-01-06 01:02:08 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'todoapp-ali';
+  todos: Todo[] = [];
+  newTodo: string;
+
+  saveTodo() {
+    if (this.newTodo) {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true
+      this.todos.push(todo);
+      this.newTodo = "";
+    } else {
+      alert('Please Enter Todo');
+    }
+  }
+
+
+delete (id:number) {
+  this.todos = this.todos.filter((v, i) => i !== id);
 }
+
+/**checkOR(id:number){
+ this.todos[id].isCompleted = !this.todos[id].isCompleted;
+ }*/
+
+deleteAll() {
+  this.todos = this.todos.filter((todos) => !todos.isCompleted);
+}
+  }
